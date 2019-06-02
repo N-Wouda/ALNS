a7e151b611c4de0804d25d2231cabc482530b320
Niels Wouda <nielswouda@gmail.com>
2019-06-02 17:05:58 +0200

    Some docstrings

--- alns/Result.py
+++ alns/Result.py
@@ -1,4 +1,11 @@
+from .State import State
+
+
 class Result:
+    """
+    Stores ALNS results. An instance of this class is returned once the
+    algorithm completes.
+    """
 
     def __init__(self, best, last):
         self._best = best
@@ -6,8 +13,24 @@ class Result:
 
     @property
     def best_state(self):
+        """
+        The best state observed during the entire iteration.
+
+        Returns
+        -------
+        State
+            The associated State object
+        """
         return self._best
 
     @property
     def last_state(self):
+        """
+        The last accepted state before the algorithm terminated.
+
+        Returns
+        -------
+        State
+            The associated State object
+        """
         return self._last

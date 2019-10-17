f5a6117d73bd2cc6eddfb2f306a303f8413e3d18
Niels Wouda <nielswouda@gmail.com>
2019-10-17 17:26:51 +0200

    Set-up Travis for Python 3.8

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "Development Status :: 5 - Production/Stable",

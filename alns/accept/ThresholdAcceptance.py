113ca6f203995fe35b8c3f41fda9d21faf5e360f
Leon Lan <leon.lanyidong@gmail.com>
2022-06-01 15:12:27 +0200

    Implemented and tested ThresholdAcceptance

---
+++ alns/accept/ThresholdAcceptance.py
@@ -0,0 +1,46 @@
+from alns.accept.RecordToRecordTravel import RecordToRecordTravel
+from alns.accept.update import update
+
+
+class ThresholdAcceptance(RecordToRecordTravel):
+    """
+    Threshold acceptance, using an updating threshold. The threshold is
+    updated as,
+
+    ``threshold = max(end_threshold, threshold - step)`` (linear)
+
+    ``threshold = max(end_threshold, step * threshold)`` (exponential)
+
+    where the initial threshold is set to ``start_threshold``.
+
+    Parameters
+    ----------
+    start_threshold
+        The initial threshold.
+    end_threshold
+        The final threshold.
+    step
+        The updating step.
+    method
+        The updating method, one of {'linear', 'exponential'}. Default
+        'linear'.
+
+    References
+    ----------
+    [1]: Santini, A., Ropke, S. & Hvattum, L.M. A comparison of acceptance
+         criteria for the adaptive large neighbourhood search metaheuristic.
+         *Journal of Heuristics* (2018) 24 (5): 783–815.
+    [2]: Dueck, G., Scheuer, T. Threshold accepting: A general purpose
+         optimization algorithm appearing superior to simulated annealing.
+         *Journal of Computational Physics* (1990) 90 (1): 161-175.
+    """
+
+    def __call__(self, rnd, best, current, candidate):
+        # This follows from the paper by Dueck and Scheueur (1990), p. 162.
+        result = (candidate.objective() - current.objective()) <= self._threshold
+
+        self._threshold = max(
+            self.end_threshold, update(self._threshold, self.step, self.method)
+        )
+
+        return result
--- alns/accept/__init__.py
+++ alns/accept/__init__.py
@@ -3,4 +3,5 @@ from .HillClimbing import HillClimbing
 from .LateAcceptanceHillClimbing import LateAcceptanceHillClimbing
 from .RandomWalk import RandomWalk
 from .RecordToRecordTravel import RecordToRecordTravel
+from .ThresholdAcceptance import ThresholdAcceptance
 from .SimulatedAnnealing import SimulatedAnnealing

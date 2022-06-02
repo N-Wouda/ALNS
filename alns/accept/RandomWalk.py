b2d4f17e266d474a7c28e05c4ef9c6ebd2f59a5a
Leon Lan <leon.lanyidong@gmail.com>
2022-06-02 08:21:21 +0200

    Improve docstrings RW and LAHC

--- alns/accept/RandomWalk.py
+++ alns/accept/RandomWalk.py
@@ -3,7 +3,7 @@ from alns.accept.AcceptanceCriterion import AcceptanceCriterion
 
 class RandomWalk(AcceptanceCriterion):
     """
-    The random walk method always accepts the candidate solution.
+    The random walk criterion always accepts the candidate solution.
     """
 
     def __call__(self, rnd, best, current, candidate):
--- alns/accept/tests/test_late_acceptance_hill_climbing.py
+++ alns/accept/tests/test_late_acceptance_hill_climbing.py
@@ -15,8 +15,8 @@ def test_raises_invalid_n_last(n_last):
 @mark.parametrize("n_last", [3, 10, 50])
 def test_late_acceptance(n_last):
     """
-    Tests if the late acceptance hill climbing method accepts a solution
-    that is better than the current solution n_last ago.
+    Tests if the late acceptance hill climbing criterion accepts a solution
+    that is better than the current solution n_last iterations ago.
     """
     lahc = LateAcceptanceHillClimbing(n_last)
 
@@ -28,7 +28,7 @@ def test_late_acceptance(n_last):
     for _ in range(n_last - 1):
         accept(Zero(), One())
 
-    # The current solution n_last ago has value 2, so the candidate
+    # The current solution n_last iterations ago has value 2, so the candidate
     # solution with value 1 should be accepted.
     assert_equal(lahc.last_objectives[0], 2)
     assert_(accept(Zero(), One()))
@@ -37,8 +37,8 @@ def test_late_acceptance(n_last):
 @mark.parametrize("n_last", [3, 10, 50])
 def test_improved_late_acceptance(n_last):
     """
-    Tests if the improved late acceptance hill climbing method accepts a
-    solution that 1) is better than the current solution n_last ago
+    Tests if the improved late acceptance hill climbing criterion accepts a
+    solution that 1) is better than the current solution n_last iterations ago
     or 2) is better than the current solution.
     """
     ilahc = LateAcceptanceHillClimbing(n_last, improved=True)
@@ -51,7 +51,7 @@ def test_improved_late_acceptance(n_last):
     for _ in range(n_last - 1):
         accept(Zero(), One())
 
-    # The current solution n_last ago has value 0 and the candidate
+    # The current solution n_last iterations ago has value 0 and the candidate
     # solution has value 1. But since the current solution has value 1,
     # the improved variant of LAHC will accept this solution.
     assert_equal(ilahc.last_objectives[0], 0)

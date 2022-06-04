af40f2d06d9dba295f57834d4863b61adc485556
Leon Lan <leon.lanyidong@gmail.com>
2022-06-04 15:16:47 +0200

    Rename and refactor GD tests

--- alns/accept/tests/test_great_deluge.py
+++ alns/accept/tests/test_great_deluge.py
@@ -43,11 +43,11 @@ def test_does_not_raise():
 # TODO Test properties if included?
 
 """
-Tests for linear variant
+Linear Great Deluge (GD)
 """
 
 
-def test_linear_threshold_update():
+def test_gd_threshold_update():
     """
     Test if the threshold is correctly updated using the linear update method.
     """
@@ -62,21 +62,21 @@ def test_linear_threshold_update():
     assert_equal(great_deluge._threshold, 0.0099)
 
 
-def test_linear_accepts_below_threshold():
+def test_gd_accepts_below_threshold():
     great_deluge = GreatDeluge(2.01, 0.5, method="linear")
 
     # Initial threshold is set at 2.01, hence Two should be accepted
     assert_(great_deluge(None, One(), Zero(), Two()))
 
 
-def test_linear_rejects_above_threshold():
+def test_gd_rejects_above_threshold():
     great_deluge = GreatDeluge(1.01, 0.5, method="linear")
 
     # Initial threshold is set at 1.01, hence Two should be rejected
     assert_(not great_deluge(None, One(), Zero(), Two()))
 
 
-def test_linear_rejects_equal_threshold():
+def test_gd_rejects_equal_threshold():
     great_deluge = GreatDeluge(2, 0.5, method="linear")
 
     # Initial threshold is set at 2, hence One should be rejected
@@ -84,14 +84,14 @@ def test_linear_rejects_equal_threshold():
 
 
 """
-Tests for non-linear variant
+Non-Linear Great Deluge
 """
 
 
-def test_non_linear_raise_zero_best():
+def test_nlgd_raise_zero_best():
     """
-    Test if an error is raised when the initial solution has value 0 and
-    the non-linear variant is used.
+    Test if an error is raised when the initial solution has value 0
+    using NLGD.
 
     Reason: the relative gap cannot be computed.
     """
@@ -101,31 +101,14 @@ def test_non_linear_raise_zero_best():
         great_deluge(None, Zero(), One(), One())
 
 
-def test_non_linear_threshold_update():
-    """
-    Test if the threshold is correctly updated using the non-linear update
-    method.
-    """
-    great_deluge = GreatDeluge(1.5, 0.1, 0.02, 1, method="non-linear")
-
-    # The first candidate is rejected and linearly increases the threshold
-    great_deluge(None, One(), Zero(), Two())
-    assert_equal(great_deluge._threshold, 1.51)
-
-    # The second candidate is accepted and exponentially decreases the threshold
-    great_deluge(None, One(), Zero(), Zero())
-    new_threshold = 1.51 * np.exp(-1) + 1
-    assert_equal(great_deluge._threshold, new_threshold)
-
-
-def test_non_linear_accepts_below_threshold():
+def test_nlgd_accepts_below_threshold():
     great_deluge = GreatDeluge(1.01, 0.5, 0.01, 1, method="non-linear")
 
     # Initial threshold is set at 1.01, hence One should be accepted
     assert_(great_deluge(None, One(), Zero(), One()))
 
 
-def test_non_linear_rejects_above_threshold():
+def test_nlgd_rejects_above_threshold():
     great_deluge = GreatDeluge(2, 0.5, 1, 1, method="non-linear")
 
     # Initial threshold is set at 2 and the current solution is Zero,
@@ -133,9 +116,24 @@ def test_non_linear_rejects_above_threshold():
     assert_(not great_deluge(None, One(), Zero(), Two()))
 
 
-def test_non_linear_accepts_improving_current():
+def test_nlgd_accepts_improving_current():
     great_deluge = GreatDeluge(2, 0.5, 1, 1, method="non-linear")
 
     # Two does not improve over the threshold (2) but does improve over the
     # current solution value (3), hence One should be accepted
     assert_(great_deluge(None, One(), Three(), Two()))
+
+
+def test_nlgd_evaluate_consecutive_solutions():
+    """
+    Test if NLGD correctly accepts and rejects consecutive solutions.
+    """
+    great_deluge = GreatDeluge(1.5, 0.1, 0.02, 2, method="non-linear")
+
+    # The first candidate is rejected (1.5 < 2). The threshold is linearly
+    # increased to 1.51. The second candidate is accepted (1.51 > 0). The
+    # threshold is exponentially decreased to 1.20. The third candidate
+    # is accepted (1.20 > 1).
+    assert_(not great_deluge(None, One(), Zero(), Two()))
+    assert_(great_deluge(None, One(), Zero(), Zero()))
+    assert_(great_deluge(None, One(), Zero(), One()))

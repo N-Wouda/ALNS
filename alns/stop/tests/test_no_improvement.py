ab09b939f6c2ce8ac64249d1bb83fc092bf8d765
Leon Lan <leon.lanyidong@gmail.com>
2022-06-04 14:26:19 +0200

    Improve tests

--- alns/stop/tests/test_no_improvement.py
+++ alns/stop/tests/test_no_improvement.py
@@ -7,29 +7,29 @@ from alns.tests.states import One, Zero, Two
 from alns.stop import NoImprovement
 
 
-@pytest.mark.parametrize("n_iterations", [-10, -100, -1000])
-def test_raise_negative_parameters(n_iterations: int):
+@pytest.mark.parametrize("max_iterations", [-10, -100, -1000])
+def test_raise_negative_parameters(max_iterations: int):
     """
-    n_iterations cannot be negative.
+    max_iterations cannot be negative.
     """
     with assert_raises(ValueError):
-        NoImprovement(n_iterations)
+        NoImprovement(max_iterations)
 
 
-@pytest.mark.parametrize("n_iterations", [0, 10, 100, 1000])
-def test_does_not_raise(n_iterations: int):
+@pytest.mark.parametrize("max_iterations", [0, 10, 100, 1000])
+def test_does_not_raise(max_iterations: int):
     """
     Non-negative integers should not raise.
     """
-    NoImprovement(n_iterations)
+    NoImprovement(max_iterations)
 
 
-def test_n_iterations():
+def test_max_iterations():
     """
-    Test if the n_iterations parameter is correctly set.
+    Test if the max_iterations parameter is correctly set.
     """
     stop = NoImprovement(3)
-    assert stop.n_iterations == 3
+    assert_equal(stop.max_iterations, 3)
 
 
 def test_first_iteration():
@@ -39,40 +39,42 @@ def test_first_iteration():
     stop = NoImprovement(100)
     rnd = RandomState()
 
-    assert not stop(rnd, Zero(), Zero())
+    assert_(not stop(rnd, Zero(), One()))
 
 
-def test_before_n_iterations():
+def test_stop_after_max_iterations():
     stop = NoImprovement(100)
     rnd = RandomState()
 
     for _ in range(100):
         assert_(not stop(rnd, Zero(), Zero()))
 
-
-def test_after_n_iterations():
-    stop = NoImprovement(100)
-    rnd = RandomState()
-
-    for _ in range(100 + 1):
-        stop(rnd, Zero(), Zero())
-
     for _ in range(100):
-        # The 102-th iteration and beyond should be stopped, because there was
-        # 1 iteration to initialize and 100 subsequent non-improving iterations.
         assert_(stop(rnd, Zero(), Zero()))
 
 
-@pytest.mark.parametrize("n_iterations", [10, 100, 1000])
-def test_reset_counter(n_iterations):
+def test_counter_value_at_stop():
     """
-    Test if the counter is reset when an improving solution is encountered.
+    Test if the counter value is equal to the passed-in max iterations
+    at the first stopping occurence.
     """
-    stop = NoImprovement(n_iterations)
+    stop = NoImprovement(100)
+
+    while not stop(RandomState(), Zero(), Zero()):
+        pass
+
+    assert_equal(stop._counter, 100)
+
+
+def test_reset_counter():
+    """
+    Test if the counter is reset when a new best solution is encountered.
+    """
+    stop = NoImprovement(100)
     rnd = RandomState()
 
-    for _ in range(n_iterations):
-        assert_(not stop(rnd, Two(), Zero()))
+    for _ in range(5):
+        assert_(not stop(rnd, One(), One()))
 
-    assert not stop(rnd, One(), Zero())
-    assert stop._counter == 0
+    assert_(not stop(rnd, Zero(), One()))
+    assert_equal(stop._counter, 0)

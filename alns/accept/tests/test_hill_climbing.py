9914eb9faae2d11273aafafcab5f3ea974fb3c2a
Leon Lan <leon.lanyidong@gmail.com>
2022-06-04 14:52:11 +0200

    Improve HC test names and refactor tests

--- alns/accept/tests/test_hill_climbing.py
+++ alns/accept/tests/test_hill_climbing.py
@@ -41,13 +41,13 @@ Late Acceptance Hill Climbing
 
 
 @mark.parametrize("n_last", [-0.01, -10, 1.5])
-def test_raises_invalid_n_last(n_last):
+def test_lahc_raises_invalid_n_last(n_last):
     with assert_raises(ValueError):
         HillClimbing(n_last=n_last)
 
 
 @mark.parametrize("n_last", [3, 10, 50])
-def test_late_acceptance(n_last):
+def test_lahc_accept(n_last):
     """
     Tests if the late acceptance hill climbing criterion accepts a solution
     that is better than the current solution n_last iterations ago.
@@ -57,19 +57,18 @@ def test_late_acceptance(n_last):
     def accept(current, candidate):
         return lahc(None, None, current, candidate)
 
-    accept(Two(), One())
-
-    for _ in range(n_last - 1):
-        accept(Zero(), One())
+    for _ in range(n_last):
+        assert_(accept(Two(), Two()))
 
     # The current solution n_last iterations ago has value 2, so the candidate
-    # solution with value 1 should be accepted.
+    # solution with value 1 should be accepted despite being worse than the
+    # current solution with value 0.
     assert_equal(lahc._last_objectives[0], 2)
     assert_(accept(Zero(), One()))
 
 
 @mark.parametrize("n_last", [3, 10, 50])
-def test_improved_late_acceptance(n_last):
+def test_ilahc_accept(n_last):
     """
     Tests if the improved late acceptance hill climbing criterion accepts a
     solution that 1) is better than the current solution n_last iterations ago
@@ -80,10 +79,8 @@ def test_improved_late_acceptance(n_last):
     def accept(current, candidate):
         return ilahc(None, None, current, candidate)
 
-    accept(Zero(), One())
-
-    for _ in range(n_last - 1):
-        accept(Zero(), One())
+    for _ in range(n_last):
+        assert_(not accept(Zero(), One()))
 
     # The current solution n_last iterations ago has value 0 and the candidate
     # solution has value 1. But since the current solution has value 1,

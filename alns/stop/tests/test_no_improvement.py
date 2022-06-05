6d4a9ecd09f339cf7bbd45adc54036e85ad40b33
Leon Lan <leon.lanyidong@gmail.com>
2022-06-05 09:55:13 +0200

    Change NI tests

--- alns/stop/tests/test_no_improvement.py
+++ alns/stop/tests/test_no_improvement.py
@@ -34,10 +34,21 @@ def test_max_iterations():
 
 def test_zero_max_iterations():
     """
-    Test if setting max_iterations to zero stops when a non-improving
-    best solution has been found.
+    Test if setting max_iterations to zero always stops.
     """
     stop = NoImprovement(0)
+    rnd = RandomState()
+
+    assert_(stop(rnd, One(), Zero()))
+    assert_(stop(rnd, Zero(), Zero()))
+
+
+def test_one_max_iterations():
+    """
+    Test if setting max_iterations to one only stops when a non-improving
+    best solution has been found.
+    """
+    stop = NoImprovement(1)
     rnd = RandomState()
 
     assert_(not stop(rnd, One(), Zero()))
@@ -45,17 +56,12 @@ def test_zero_max_iterations():
     assert_(stop(rnd, Zero(), Zero()))
 
 
-def test_first_iteration():
+def test_n_max_iterations_non_improving():
     """
-    Test if the first iteration does not stop.
+    Test if setting max_iterations to N correctly stops with non-improving
+    solutions. The first N iterations should not stop. Beyond that, the
+    the criterion should stop.
     """
-    stop = NoImprovement(100)
-    rnd = RandomState()
-
-    assert_(not stop(rnd, Zero(), One()))
-
-
-def test_stop_after_max_iterations():
     stop = NoImprovement(100)
     rnd = RandomState()
 
@@ -66,14 +72,21 @@ def test_stop_after_max_iterations():
         assert_(stop(rnd, Zero(), Zero()))
 
 
-def test_counter_value_at_stop():
+def test_n_max_iterations_with_single_improvement():
     """
-    Test if the counter value is equal to the passed-in max iterations
-    at the first stopping occurence.
+    Test if setting max_iterations to N correctly stops with a sequence
+    of solutions, where the third solution is improving and the other solutions
+    are non-improving. The first N+2 iterations should not stop. Beyond that,
+    the criterion should stop.
     """
     stop = NoImprovement(100)
+    rnd = RandomState()
 
-    while not stop(RandomState(), Zero(), Zero()):
-        pass
+    for _ in range(2):
+        assert_(not stop(rnd, One(), Zero()))
 
-    assert_equal(stop._counter, 100)
+    for _ in range(100):
+        assert_(not stop(rnd, Zero(), Zero()))
+
+    for _ in range(100):
+        assert_(stop(rnd, Zero(), Zero()))

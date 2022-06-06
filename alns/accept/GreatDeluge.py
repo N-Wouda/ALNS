6daa99678987b0fbdb9fecb9e4f69b09921ad1d7
Leon Lan <leon.lanyidong@gmail.com>
2022-06-06 11:15:36 +0200

    Separate GD

--- alns/accept/GreatDeluge.py
+++ alns/accept/GreatDeluge.py
@@ -1,4 +1,3 @@
-import math
 from typing import Optional
 
 from alns.accept.AcceptanceCriterion import AcceptanceCriterion
@@ -13,28 +12,19 @@ class GreatDeluge(AcceptanceCriterion):
     ``threshold = alpha * initial.objective()``
 
     where `initial` is the initial solution passed-in to ALNS, inferred
-    from the best solution at the first iteration.
+    from the best solution at the first iteration. The threshold is updated in
+    each iteration as
 
-    There are two variants of the GD criterion: 1) linear and 2) non-linear,
-    proposed in [2] and [3], respectively. The main difference between these
-    variants is the threshold updating scheme, see the `_update` method for
-    more details. Moreover, in the non-linear variant, candidate solutions that
-    improve the current solution are always accepted.
+    ``threshold = threshold - beta * (threshold - candidate.objective()``
 
     The implementation is based on the description in [1].
 
     Parameters
     ----------
     alpha
-        The factor to compute the initial threshold
+        Factor to compute the initial threshold
     beta
-        A factor used for updating the threshold
-    gamma
-        A factor used for updating the threshold (only in non-linear GD)
-    delta
-        A factor used for updating the threshold (only in non-linear GD)
-    method
-        The updating method, one of {'linear', 'non-linear'}. Default 'linear'.
+        Factor used for updating the threshold
 
     References
     ----------
@@ -44,98 +34,41 @@ class GreatDeluge(AcceptanceCriterion):
     [2]: Dueck, G. New optimization heuristics: The great deluge algorithm and
          the record-to-record travel. *Journal of Computational Physics* (1993)
          104 (1): 86-92.
-    [3]: Landa-Silva, D., & Obit, J. H. Great deluge with non-linear decay rate
-         for solving course timetabling problems. *4th international IEEE
-         conference intelligent systems* (2008) Vol. 1: 8-11.
     """
 
     def __init__(
         self,
         alpha: float,
         beta: float,
-        gamma: Optional[float] = None,
-        delta: Optional[float] = None,
-        method: str = "linear",
     ):
         if alpha <= 1:
             raise ValueError("Alpha must be larger than 1.")
 
-        if beta <= 0 or beta >= 1:
+        if not (0 < beta < 1):
             raise ValueError("Beta must be in (0, 1).")
-
-        if (
-            gamma is None or delta is None or gamma <= 0 or delta <= 0
-        ) and method == "non-linear":
-            raise ValueError("Gamma and delta must be non-negative.")
-
-        if method not in ["linear", "non-linear"]:
-            raise ValueError(f"Method {method} not understrood.")
 
         self._alpha = alpha
         self._beta = beta
-        self._method = method
-        self._threshold = None
+        self._threshold: Optional[float] = None
 
-        if self._method == "non-linear":
-            self._gamma = gamma
-            self._delta = delta
+    @property
+    def alpha(self):
+        return self._alpha
+
+    @property
+    def beta(self):
+        return self._beta
 
     def __call__(self, rnd, best, current, candidate):
         if self._threshold is None:
             self._threshold = self._alpha * best.objective()
 
-            if self._method == "non-linear" and self._threshold == 0:
-                raise ValueError(
-                    "Initial solution cannot have zero objective value."
-                )
-
-        result = candidate.objective() < self._threshold
-
-        if self._method == "non-linear":
-            result |= candidate.objective() < current.objective()
+        res = candidate.objective() < self._threshold
 
         self._threshold = self._update(best, current, candidate)
 
-        return result
+        return res
 
     def _update(self, best, current, candidate):
-        """
-        Return the new threshold value based on the selected GD variant.
-
-        In the linear GD variant, the threshold is updated in each iteration as
-
-        ``threshold = threshold - beta * (threshold - candidate.objective())``
-
-        TODO Improve this description.
-        In the non-linear variant, the updating method is more involved.
-        - First, the relative gap between the candidate solution and threshold
-          is computed.
-          - If this relative gap is less than `beta` parameter, meaning
-            that 1) the candidate value was higher than the threshold or 2)
-            improving less than beta relative to the threshold, then the
-            threshold is linearly increased (using the `gamma` parameter).
-          - Otherwise, the threshold is exponentially decreased (using the
-            `delta` parameter).
-        """
-
-        if self._method == "linear":
-            result = self._threshold - self._beta * (
-                self._threshold - candidate.objective()
-            )
-
-        elif self._method == "non-linear":
-            rel_gap = (
-                self._threshold - candidate.objective()
-            ) / self._threshold
-
-            if rel_gap < self._beta:
-                result = self._threshold + self._gamma * abs(
-                    candidate.objective() - self._threshold
-                )
-            else:
-                result = (
-                    self._threshold * math.exp(-self._delta * best.objective())
-                    + best.objective()
-                )
-
-        return result
+        change = self._beta * (candidate.objective() - self._threshold)
+        return self._threshold + change
--- alns/accept/tests/test_great_deluge.py
+++ alns/accept/tests/test_great_deluge.py
@@ -2,14 +2,13 @@ import numpy as np
 import numpy.random as rnd
 from numpy.testing import (
     assert_,
-    assert_almost_equal,
     assert_equal,
     assert_raises,
 )
 from pytest import mark
 
 from alns.accept import GreatDeluge
-from alns.tests.states import One, Zero, Two, VarObj
+from alns.tests.states import One, Zero, Two
 
 
 def test_raise_invalid_parameters():
@@ -19,39 +18,28 @@ def test_raise_invalid_parameters():
     with assert_raises(ValueError):
         GreatDeluge(2, 1)  # beta cannot be outside of (0, 1)
 
-    with assert_raises(ValueError):
-        # If method=non-linear, then gamma cannot be <= 0
-        GreatDeluge(2, 0.5, 0, 0.5, method="non-linear")
-
-    with assert_raises(ValueError):
-        # If method=non-linear, then delta cannot be <= 0
-        GreatDeluge(2, 0.5, 0.5, 0, method="non-linear")
-
-    with assert_raises(ValueError):
-        GreatDeluge(2, 0.5, 0.5, 0.5, method="exponential")  # Invalid method
-
 
 def test_does_not_raise():
     GreatDeluge(2, 0.5)
 
-    # Passing in gamma and delta without method='non-linear' should work
-    GreatDeluge(2, 0.5, 0, 0)
 
-    GreatDeluge(2, 0.5, 0.5, 0.5, method="non-linear")
-
-
-# TODO Test properties if included?
-
-"""
-Linear Great Deluge (GD)
-"""
+@mark.parametrize("alpha", np.arange(1.1, 10, 1))
+def test_alpha(alpha):
+    great_deluge = GreatDeluge(alpha, 0.1)
+    assert_equal(great_deluge.alpha, alpha)
 
 
-def test_gd_threshold_update():
+@mark.parametrize("beta", np.arange(0.1, 1, 0.1))
+def test_beta(beta):
+    great_deluge = GreatDeluge(2, beta)
+    assert_equal(great_deluge.beta, beta)
+
+
+def test_threshold_update():
     """
     Test if the threshold is correctly updated using the linear update method.
     """
-    great_deluge = GreatDeluge(2, 0.01, method="linear")
+    great_deluge = GreatDeluge(2, 0.01)
 
     # The first candidate is rejected and increases the threshold
     great_deluge(None, Zero(), Zero(), One())
@@ -62,79 +50,22 @@ def test_gd_threshold_update():
     assert_equal(great_deluge._threshold, 0.0099)
 
 
-def test_gd_accepts_below_threshold():
-    great_deluge = GreatDeluge(2.01, 0.5, method="linear")
+def test_accepts_below_threshold():
+    great_deluge = GreatDeluge(2.01, 0.5)
 
     # Initial threshold is set at 2.01, hence Two should be accepted
     assert_(great_deluge(None, One(), Zero(), Two()))
 
 
-def test_gd_rejects_above_threshold():
-    great_deluge = GreatDeluge(1.01, 0.5, method="linear")
+def test_rejects_above_threshold():
+    great_deluge = GreatDeluge(1.01, 0.5)
 
     # Initial threshold is set at 1.01, hence Two should be rejected
     assert_(not great_deluge(None, One(), Zero(), Two()))
 
 
-def test_gd_rejects_equal_threshold():
-    great_deluge = GreatDeluge(2, 0.5, method="linear")
+def test_rejects_equal_threshold():
+    great_deluge = GreatDeluge(2, 0.5)
 
     # Initial threshold is set at 2, hence One should be rejected
     assert_(not great_deluge(None, One(), Zero(), Two()))
-
-
-"""
-Non-Linear Great Deluge
-"""
-
-
-def test_nlgd_raise_zero_best():
-    """
-    Test if an error is raised when the initial solution has value 0
-    using NLGD.
-
-    Reason: the relative gap cannot be computed with zero threshold.
-    """
-    great_deluge = GreatDeluge(2, 0.1, 0.01, 1, method="non-linear")
-
-    with assert_raises(ValueError):
-        great_deluge(None, Zero(), One(), One())
-
-
-def test_nlgd_accepts_below_threshold():
-    great_deluge = GreatDeluge(1.01, 0.5, 0.01, 1, method="non-linear")
-
-    # Initial threshold is set at 1.01, hence One should be accepted
-    assert_(great_deluge(None, One(), Zero(), One()))
-
-
-def test_nlgd_rejects_above_threshold():
-    great_deluge = GreatDeluge(2, 0.5, 1, 1, method="non-linear")
-
-    # Initial threshold is set at 2 and the current solution is Zero,
-    # hence Two should be rejected
-    assert_(not great_deluge(None, One(), Zero(), Two()))
-
-
-def test_nlgd_accepts_improving_current():
-    great_deluge = GreatDeluge(2, 0.5, 1, 1, method="non-linear")
-
-    # Candidate is does not improve the threshold (2 == 2) but does improve the
-    # current solution value (2 < 3), hence candidate should be accepted
-    assert_(great_deluge(None, One(), VarObj(3), Two()))
-
-
-def test_nlgd_evaluate_consecutive_solutions():
-    """
-    Test if NLGD correctly accepts and rejects consecutive solutions.
-    """
-    great_deluge = GreatDeluge(1.5, 0.1, 0.02, 2, method="non-linear")
-
-    # The first is above the threshold (2 > 1.5) so the first should be
-    # rejected. The threshold is then linearly increased to 1.51. The second
-    # candidate is below the threshold (0 < 1.51), so the second should be
-    # accepted. The threshold is then exponentially decreased to 1.20. The third
-    # candidate is below the theshold (1 < 1.20) and is accepted.
-    assert_(not great_deluge(None, One(), Zero(), Two()))
-    assert_(great_deluge(None, One(), Zero(), Zero()))
-    assert_(great_deluge(None, One(), Zero(), One()))

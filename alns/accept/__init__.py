caf0594994c7dcdbe71a03364e7815f93b3907b7
Leon <30997278+leonlan@users.noreply.github.com>
2022-06-06 21:12:37 +0200

    WorseAccept acceptance criterion (#78)
    * Add WorseAccept
    
    * WA call refactor and test improvements

--- alns/accept/__init__.py
+++ alns/accept/__init__.py
@@ -2,4 +2,5 @@ from .AcceptanceCriterion import AcceptanceCriterion
 from .HillClimbing import HillClimbing
 from .RecordToRecordTravel import RecordToRecordTravel
 from .SimulatedAnnealing import SimulatedAnnealing
+from .WorseAccept import WorseAccept
 from .RandomWalk import RandomWalk

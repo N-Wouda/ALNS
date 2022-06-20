c50a530d9ec8dfc61050bf816b029763508c802c
Leon <30997278+leonlan@users.noreply.github.com>
2022-06-20 10:32:28 +0200

    ThresholdAcceptance acceptance criterion (#80)
    * ThresholdAcceptance acceptance criterion
    
    * RRT autofit
    
    * Add tests for RRT autofit coverage
    
    * Parametrize tests for RRT
    
    * Refactor RRT
    
    * Rename ThresholdAcceptance to ThresholdAccept
    
    Co-authored-by: Niels Wouda <N-Wouda@users.noreply.github.com>

--- alns/accept/tests/test_record_to_record_travel.py
+++ alns/accept/tests/test_record_to_record_travel.py
@@ -1,84 +1,64 @@
 import numpy.random as rnd
-from numpy.testing import assert_, assert_equal, assert_raises
+from numpy.testing import (
+    assert_,
+    assert_almost_equal,
+    assert_equal,
+    assert_raises,
+)
+from pytest import mark
 
 from alns.accept import RecordToRecordTravel
 from alns.tests.states import One, Zero
 
 
-def test_raises_negative_parameters():
-    """
-    Record-to-record travel does not work with negative parameters, so those
-    should not be accepted.
-    """
-    with assert_raises(ValueError):  # start threshold cannot be negative
-        RecordToRecordTravel(-1, 1, 1)
-
-    with assert_raises(ValueError):  # nor can the end threshold
-        RecordToRecordTravel(1, -1, 1)
-
-    with assert_raises(ValueError):  # nor the updating step
-        RecordToRecordTravel(1, 1, -1)
-
-
-def test_raises_explosive_step():
-    """
-    For exponential updating, the step parameter must not be bigger than one,
-    as that would result in an explosive threshold.
-    """
+@mark.parametrize(
+    "start, end, step, method",
+    [
+        (-1, 1, 1, "linear"),  # start threshold cannot be negative
+        (1, -1, 1, "linear"),  # nor can the end threshold
+        (1, 1, -1, "linear"),  # nor the updating step
+        (0, 1, 1, "linear"),  # start threshold cannot be < end threshold
+        (2, 1, 2, "exponential"),  # step cannot be > 1 with exponential
+        (1, 1, 1, "non-linear"),  # method must be 'linear' or 'exponential'
+    ],
+)
+def test_raises_invalid_parameters(start, end, step, method):
     with assert_raises(ValueError):
-        RecordToRecordTravel(2, 1, 2, "exponential")
-
-    RecordToRecordTravel(2, 1, 1, "exponential")  # boundary should be fine
+        RecordToRecordTravel(start, end, step, method)
 
 
-def test_threshold_boundary():
+@mark.parametrize(
+    "start, end, step, method",
+    [
+        (0, 0, 1, "linear"),  # boundary start and end threshold
+        (0.5, 0.5, 1, "linear"),  # non-boundary equal start and end
+        (10, 0, 0, "linear"),  # boundary step
+        (1, 0.5, 1, "exponential"),  # boundary step exponential
+        (1000, 0.5, 0.1, "linear"),  # regular parameters
+    ],
+)
+def test_no_raise_valid_parameters(start, end, step, method):
+    RecordToRecordTravel(start, end, step, method)
+
+
+@mark.parametrize(
+    "start, end, step, method",
+    [
+        (1, 0, 1, "linear"),
+        (0.5, 0.5, 1, "linear"),
+        (10, 5, 0.001, "linear"),
+        (1, 0.5, 1, "exponential"),
+    ],
+)
+def test_properties(start, end, step, method):
     """
-    The boundary case for the end threshold parameter is at zero, which should
-    be accepted.
+    Test if the properties are correctly set.
     """
-    RecordToRecordTravel(1, 0, 1)
-
-
-def test_raises_start_smaller_than_end():
-    """
-    The initial threshold at the start should be bigger (or equal) to the end
-    threshold.
-    """
-    with assert_raises(ValueError):
-        RecordToRecordTravel(0, 1, 1)
-
-    RecordToRecordTravel(1, 1, 1)  # should not raise for equality
-
-
-def test_does_not_raise():
-    """
-    This set of parameters should work correctly.
-    """
-    RecordToRecordTravel(10, 5, 2)
-
-
-def test_step():
-    """
-    Tests if the step parameter is correctly set.
-    """
-    for step in range(100):
-        assert_equal(RecordToRecordTravel(1, 1, step).step, step)
-
-
-def test_start_threshold():
-    """
-    Tests if the start_threshold parameter is correctly set.
-    """
-    for start in range(100):
-        assert_equal(RecordToRecordTravel(start, 0, 1).start_threshold, start)
-
-
-def test_end_threshold():
-    """
-    Tests if the end_threshold parameter is correctly set.
-    """
-    for end in range(100):
-        assert_equal(RecordToRecordTravel(100, end, 1).end_threshold, end)
+    rrt = RecordToRecordTravel(start, end, step, method)
+    assert_equal(rrt.start_threshold, start)
+    assert_equal(rrt.end_threshold, end)
+    assert_equal(rrt.step, step)
+    assert_equal(rrt.method, method)
 
 
 def test_accepts_better():
@@ -123,3 +103,57 @@ def test_exponential_threshold_update():
     # second should be rejected.
     assert_(record_travel(rnd.RandomState(), Zero(), Zero(), One()))
     assert_(not record_travel(rnd.RandomState(), Zero(), Zero(), One()))
+
+
+@mark.parametrize(
+    "init_obj, start_gap, end_gap, n_iters, method",
+    [
+        (1, -1, 0, 1, "linear"),  # negative start
+        (1, 0, -1, 1, "linear"),  # negative end
+        (1, 0.5, 1, 1, "linear"),  # start < end
+        (1, 0, 0, 0, "linear"),  # non-positive n_iters
+        (1, 0, 0, 1, "nonlinear"),  # invalid method
+    ],
+)
+def test_autofit_raises_for_invalid_inputs(
+    init_obj: float,
+    start_gap: float,
+    end_gap: float,
+    n_iters: int,
+    method: str,
+):
+    with assert_raises(ValueError):
+        RecordToRecordTravel.autofit(
+            init_obj, start_gap, end_gap, n_iters, method
+        )
+
+
+@mark.parametrize(
+    "init_obj, start_gap, end_gap, n_iters, method, rrt_start, rrt_end, rrt_step",
+    [
+        (1, 1, 0, 1, "linear", 1, 0, 1),
+        (10, 0.1, 0, 10, "linear", 1, 0, 0.1),
+        (100, 0.5, 0.5, 10, "linear", 50, 50, 0),
+        (1, 1, 0.1, 1, "exponential", 1, 0.1, 0.1),
+        (10, 0.1, 0.01, 10, "exponential", 1, 0.1, 0.79432),
+        (100, 0.05, 0.05, 10, "exponential", 5, 5, 1),
+    ],
+)
+def test_autofit_on_several_examples(
+    init_obj: float,
+    start_gap: float,
+    end_gap: float,
+    n_iters: int,
+    method: str,
+    rrt_start: float,
+    rrt_end: float,
+    rrt_step: float,
+):
+    rrt = RecordToRecordTravel.autofit(
+        init_obj, start_gap, end_gap, n_iters, method
+    )
+
+    assert_almost_equal(rrt.start_threshold, rrt_start)
+    assert_almost_equal(rrt.end_threshold, rrt_end)
+    assert_almost_equal(rrt.step, rrt_step, decimal=3)
+    assert_equal(rrt.method, method)

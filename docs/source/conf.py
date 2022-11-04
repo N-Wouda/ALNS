88d4236c51b53a77c2db107036264efefeea4e5b
Niels Wouda <nielswouda@gmail.com>
2022-11-04 20:50:39 +0100

    Update configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,37 +1,37 @@
-# Configuration file for the Sphinx documentation builder.
+import datetime
 
 # -- Project information
 
-# TODO fix this up nicely
+today = datetime.date.today()
 
-project = 'ALNS'
-copyright = '2022, Niels Wouda and contributors'
-author = 'Niels Wouda and contributors'
+project = "ALNS"
+copyright = f"2019 -- {today.year}, Niels Wouda and contributors"
+author = "Niels Wouda and contributors"
 
-release = '0.1'
-version = '0.1.0'
+release = "0.1"
+version = "0.1.0"
 
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
 ]
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
-intersphinx_disabled_domains = ['std']
+intersphinx_disabled_domains = ["std"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # -- Options for EPUB output
-epub_show_urls = 'footnote'
+epub_show_urls = "footnote"

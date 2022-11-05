4e2f7f9c23a281b58574edb8e0fa7db6e2670bfe
l-lan <l.lan@vu.nl>
2022-11-05 13:32:15 +0100

    Indent deprecation message

--- alns/weights/__init__.py
+++ alns/weights/__init__.py
@@ -3,6 +3,9 @@ import warnings
 from alns.select import RouletteWheel as SimpleWeights
 from alns.select import SegmentedRouletteWheel as SegmentedWeights
 
-message = "alns.weights has been deprecated in favour of alns.select. \
-See https://github.com/N-Wouda/ALNS/issues/74 for details."
+message = (
+    "alns.weights has been deprecated in favour of alns.select."
+    "See https://github.com/N-Wouda/ALNS/issues/74 for details."
+)
+
 warnings.warn(message, DeprecationWarning, stacklevel=2)

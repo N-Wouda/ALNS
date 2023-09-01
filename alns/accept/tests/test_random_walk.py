04991b6b2d21437992da2da415a3f98958ec9e2c
Leon Lan <30997278+leonlan@users.noreply.github.com>
2023-09-01 10:16:40 +0200

    Rename acceptance criteria (#167)

--- alns/accept/tests/test_random_walk.py
+++ alns/accept/tests/test_random_walk.py
@@ -1,30 +1,30 @@
 import numpy.random as rnd
 from numpy.testing import assert_
 
-from alns.accept import RandomWalk
+from alns.accept import AlwaysAccept
 from alns.tests.states import One, Zero
 
 
 def test_accepts_better():
     """
-    Tests if the random walk method accepts a better solution.
+    Tests if the always accept method accepts a better solution.
     """
-    random_walk = RandomWalk()
-    assert_(random_walk(rnd.RandomState(), One(), One(), Zero()))
+    always_accept = AlwaysAccept()
+    assert_(always_accept(rnd.RandomState(), One(), One(), Zero()))
 
 
 def test_accepts_worse():
     """
-    Tests if the random walk method accepts a worse solution.
+    Tests if the always accept method accepts a worse solution.
     """
-    random_walk = RandomWalk()
-    assert_(random_walk(rnd.RandomState(), Zero(), Zero(), One()))
+    always_accept = AlwaysAccept()
+    assert_(always_accept(rnd.RandomState(), Zero(), Zero(), One()))
 
 
 def test_accepts_equal():
     """
-    Tests if the random walk method accepts a solution that results in the
+    Tests if the always accept method accepts a solution that results in the
     same objective value.
     """
-    random_walk = RandomWalk()
-    assert_(random_walk(rnd.RandomState(), Zero(), Zero(), Zero()))
+    always_accept = AlwaysAccept()
+    assert_(always_accept(rnd.RandomState(), Zero(), Zero(), Zero()))
